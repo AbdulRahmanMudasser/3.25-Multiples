3320a0d7f0f553d3d87ac35c48713fae6ff4a86c
AbdulRahmanMudasser <mrhprince7@gmail.com>
2023-03-23 16:29:02 +0500

    problem completed

--- Multiples/Program.cs
+++ Multiples/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Multiples
+{
+    static void Main(string [] args)
+    {
+        // declaring variables
+        int firstNumber;
+        int secondNumber;
+
+        // prompting user to get input for first number
+        Console.Write("Enter First Number:  ");
+        firstNumber = Convert.ToInt32 (Console.ReadLine());
+
+        // prompting user to get input for second number
+        Console.Write("Enter Second Number:  ");
+        secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();    // just for spacing purpose
+
+        // if the second number is fully divisible by first number than it a multiple of second number
+        if (firstNumber % secondNumber == 0)
+        {
+            Console.WriteLine(firstNumber + " Is A Multiple Of " + secondNumber);
+        }
+
+        else
+        {
+            Console.WriteLine(firstNumber + " Is Not A Multiple Of " + secondNumber);
+        }
+    }
+}
